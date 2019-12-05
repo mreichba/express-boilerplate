4326da567f9303dadbb1f9adf9bf6e6205a22b41
Mason Reichbauer <mreichba@mail.usf.edu>
2019-12-05 11:27:07 -0500

    fixed semi

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
-const { NODE_ENV } = require('./config')
+const { NODE_ENV } = require('./config');
 const app = express();
 
 const morganOption = (NODE_ENV === 'production')
